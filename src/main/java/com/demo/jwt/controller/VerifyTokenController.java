54381f134cb76c90dded4eb34d0220fc9e6d0dcd
Serendipity <jacksparrow414@163.com>
2021-12-01 16:04:34 +0800

    refactor code

--- src/main/java/com/demo/jwt/controller/VerifyTokenController.java
+++ src/main/java/com/demo/jwt/controller/VerifyTokenController.java
@@ -1,6 +1,7 @@
 package com.demo.jwt.controller;
 
 import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Calendar;
 
 
 @RestController
@@ -26,6 +29,24 @@ public class VerifyTokenController {
     @SneakyThrows
     public boolean verifyToken(@RequestHeader("Authorization") String token) {
         SignedJWT parse = SignedJWT.parse(token);
-        return parse.verify(verifier);
+        if (!parse.verify(verifier)) {
+            throw new RuntimeException("invalid token");
+        }
+        verifyClaimsSet(parse.getJWTClaimsSet());
+        return true;
+    }
+
+    /**
+     * 所有验证在这里进行.
+     * @param jwtClaimsSet
+     */
+    private void verifyClaimsSet(final JWTClaimsSet jwtClaimsSet) {
+        boolean result = false;
+        if (Calendar.getInstance().getTime().before(jwtClaimsSet.getExpirationTime())) {
+            result = true;
+        }
+        if (!result) {
+            throw new RuntimeException("token expired");
+        }
     }
 }
